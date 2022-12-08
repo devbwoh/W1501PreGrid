d3680ba95c29869e5cabb73ff4b7a163c3feafb3
BWOH <bwoh@outlook.kr>
2022-12-08 21:37:52 +0900

    이동

--- W1501PreGrid/MainWindow.xaml.cs
+++ W1501PreGrid/MainWindow.xaml.cs
@@ -24,5 +24,34 @@ namespace W1501PreGrid
         {
             InitializeComponent();
         }
+
+        private void OnMoveLeft(object sender, RoutedEventArgs e)
+        {
+            int x = Grid.GetColumn(Ball);
+            if (x < 1) 
+                return;
+            Grid.SetColumn(Ball, x - 1);
+        }
+        private void OnMoveRight(object sender, RoutedEventArgs e)
+        {
+            int x = Grid.GetColumn(Ball);
+            if (x > Board.ColumnDefinitions.Count - 2)
+                return;
+            Grid.SetColumn(Ball, x + 1);
+        }
+        private void OnMoveUp(object sender, RoutedEventArgs e)
+        {
+            int y = Grid.GetRow(Ball);
+            if (y < 1)
+                return;
+            Grid.SetRow(Ball, y - 1);
+        }
+        private void OnMoveDown(object sender, RoutedEventArgs e)
+        {
+            int y = Grid.GetRow(Ball);
+            if (y > Board.RowDefinitions.Count - 3)
+                return;
+            Grid.SetRow(Ball, y + 1);
+        }
     }
 }
